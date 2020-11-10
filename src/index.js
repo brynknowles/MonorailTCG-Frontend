1d0690c4c6c3a6c44ea9f9ac2bcae5f354816ff6
brynknowles <bryn.knowles@gmail.com>
2020-11-10 17:52:40 -0500

    added event listeners to buttons

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ console.log("plswork")
 //dom elements
 const indexCardUl = document.querySelector("#index-cards")
 
+const navBar = document.querySelector(".nav-bar")
+const loginLi = document.querySelector("#login")
+const profileBtn = document.querySelector("#profile")
+const signupBtn = document.querySelector("#signup")
+const newcardBtn = document.querySelector("#newcard")
+const logoutBtn = document.querySelector("#logout")
 
 //render functions
 // function renderAllCards() {
@@ -31,7 +37,59 @@ const renderOneCard = cardObj => {
     indexCardUl.append(li)
 }
 
-//fetches
+// Event Listeners
+
+// navBar.addEventListener("click", function(event) {
+//     event.preventDefault()
+//     if (event.target.matches("#login"))
+//         console.log("clicked")
+// })
+
+loginLi.addEventListener("click", event => {
+    event.preventDefault()
+    console.log("login")
+
+})
+
+profileBtn.addEventListener("click", event => {
+    event.preventDefault()
+    console.log("My Profile")
+    // when a user clicks on the profile button
+    // the profile page of the user is displayed in the main
+    // all of the cards of that user will be displayed on the page
+
+    // when the user clicks the profile button
+    // render each of the user's cards
+    // slap them on the DOM
+    
+    // write a function to render the user's cards/show page inside the div#card-container
+        // if card.user_id === user.id
+        // render card(s) to page
+    
+})
+
+signupBtn.addEventListener("click", event => {
+    event.preventDefault()
+    console.log("signup")
+    
+})
+
+newcardBtn.addEventListener("click", event => {
+    event.preventDefault()
+    console.log("newcard")
+    
+})
+
+logoutBtn.addEventListener("click", event => {
+    event.preventDefault()
+    console.log("logout")
+    
+})
+
+
+
+// fetches
+
 const getAllCards = () => {
     fetch('http://localhost:3000/api/v1/cards')
     .then(r => r.json())
@@ -45,11 +103,14 @@ const getCard = id => {
     //replace clog with renderOneCard(cardObj)
 }
 
-function getUser() {
-    fetch('http://localhost:3000/api/v1/users/2')
+const getUser = id => {
+    fetch(`http://localhost:3000/api/v1/users/${id}`)
     .then(r => r.json())
     .then(data => console.log(data))
 }
-getUser()
+
+// initialize
+
+getUser(2)
 getCard(2)
 getAllCards()
