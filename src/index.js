a682c82cea6d1058b0e3070afa105e51426cfdc8
liamH47 <liamhanafee@gmail.com>
2020-11-11 15:27:34 -0500

    trying to load user

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 console.log("plswork")
 
+//plans for today
+//create forms
+//set up my profile click function
+//when clicked
+//remove all cards from card container
+//render cards where currentUserId === card.user_id
+// change p tag with all cards .textContent = user.name eg bryn's cards/profile
 
 //dom elements
 const indexCardUl = document.querySelector("#index-cards")
@@ -10,6 +17,11 @@ const profileBtn = document.querySelector("#profile")
 const signupBtn = document.querySelector("#signup")
 const newcardBtn = document.querySelector("#newcard")
 const logoutBtn = document.querySelector("#logout")
+const cardContainer = document.querySelector("#card-container")
+const profileDiv = document.querySelector("#profile-div")
+const loginForm = document.querySelector("#login-form")
+
+let currentUserId = null
 
 //render functions
 // function renderAllCards() {
@@ -17,6 +29,41 @@ const logoutBtn = document.querySelector("#logout")
 // }
 const renderAllCards = cardArray => {
     cardArray.forEach(cardObj => renderOneCard(cardObj))
+
+
+}
+const renderUserCard = userCardObj => {
+    const li = document.createElement('li')
+    li.setAttribute("class", "card")
+    li.innerHTML = `
+                    <div class="content">
+                        <h4>${userCardObj.character}</h4>
+                    </div>
+                    <div>
+                        <img src=${userCardObj.image}>
+                    </div>
+
+                    <div>
+                        <p class="quote">${userCardObj.quote}</p>
+                    </div>
+    `
+
+
+    indexCardUl.append(li)
+}
+
+const renderAllUsers = userArray => {
+    userArray.forEach(renderUser(userObj))
+}
+
+const renderUser = userObj => {
+    // console.log(userObj)
+    profileDiv.innerHTML = `
+        <p>${userObj.name} </p>
+        <p>${userObj.age} </p>
+    `
+    indexCardUl.innerHTML = ``
+    userObj.cards.forEach(renderUserCard)
 }
 
 const renderOneCard = cardObj => {
@@ -34,26 +81,32 @@ const renderOneCard = cardObj => {
                         <p class="quote">${cardObj.quote}</p>
                     </div>
     `
+
+
     indexCardUl.append(li)
+
+
 }
 
-// Event Listeners
+//change login to have a form textarea under it
+//user types in email and submits
+//if email matches any users emails, currentUserId = that user id
+// then in our event listener, we can call getUser(currentUserId)
 
-// navBar.addEventListener("click", function(event) {
-//     event.preventDefault()
-//     if (event.target.matches("#login"))
-//         console.log("clicked")
-// })
-
-loginLi.addEventListener("click", event => {
+loginForm.addEventListener("submit", event => {
     event.preventDefault()
+    // debugger
     console.log("login")
-
+    //const email = event.target.email.value 
+    //if email.matches userObj.email
+    //render that user's profile
+    // const userObj = 
+    getAllUsers()
 })
-
 profileBtn.addEventListener("click", event => {
     event.preventDefault()
     console.log("My Profile")
+
     // when a user clicks on the profile button
     // the profile page of the user is displayed in the main
     // all of the cards of that user will be displayed on the page
@@ -65,8 +118,12 @@ profileBtn.addEventListener("click", event => {
     // write a function to render the user's cards/show page inside the div#card-container
         // if card.user_id === user.id
         // render card(s) to page
-    
+    indexCardUl.innerHTML = ``
+
+//    getUser(2)
+    //write function outside to render user cards
 })
+
 
 signupBtn.addEventListener("click", event => {
     event.preventDefault()
@@ -86,9 +143,15 @@ logoutBtn.addEventListener("click", event => {
     
 })
 
-
+//fetch all users
+//
 
 // fetches
+const getAllUsers = () => {
+    fetch('http://localhost:3000/api/v1/users')
+    .then(r => r.json())
+    .then(userArray => renderAllUsers(userArray))
+}
 
 const getAllCards = () => {
     fetch('http://localhost:3000/api/v1/cards')
@@ -106,11 +169,11 @@ const getCard = id => {
 const getUser = id => {
     fetch(`http://localhost:3000/api/v1/users/${id}`)
     .then(r => r.json())
-    .then(data => console.log(data))
+    .then(userObj => renderUser(userObj))
 }
 
 // initialize
 
-getUser(2)
+// getUser(2)
 getCard(2)
 getAllCards()
