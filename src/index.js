6a6e39549a737c3b54a04904ed36b621798487cb
brynknowles <bryn.knowles@gmail.com>
2020-11-11 17:54:14 -0500

    got non-dynamic user profile to show

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-console.log("plswork")
+// console.log("plswork")
 
 //plans for today
 //create forms
@@ -8,101 +8,122 @@ console.log("plswork")
 //render cards where currentUserId === card.user_id
 // change p tag with all cards .textContent = user.name eg bryn's cards/profile
 
-//dom elements
-const indexCardUl = document.querySelector("#index-cards")
 
-const navBar = document.querySelector(".nav-bar")
-const loginLi = document.querySelector("#login")
+// ********** DOM ELEMENTS **********
+
+const allCardsUl = document.querySelector("#all-cards")
+
+const userContainer = document.querySelector("#user-container")
+
+const navBar = document.querySelector(".navBar")
+const loginForm = document.querySelector("#login-form")
 const profileBtn = document.querySelector("#profile")
 const signupBtn = document.querySelector("#signup")
 const newcardBtn = document.querySelector("#newcard")
 const logoutBtn = document.querySelector("#logout")
-const cardContainer = document.querySelector("#card-container")
-const profileDiv = document.querySelector("#profile-div")
-const loginForm = document.querySelector("#login-form")
+const homeBtn = document.querySelector("#homepage")
+
+const pageTitle = document.querySelector("#title")
+
+const cardCollection = document.querySelector("#card-collection")
 
 let currentUserId = null
 
-//render functions
-// function renderAllCards() {
-//     cardObj.forEach(renderOneCard)
-// }
-const renderAllCards = cardArray => {
-    cardArray.forEach(cardObj => renderOneCard(cardObj))
+// ********** RENDER FUNCTIONS **********
 
-
-}
 const renderUserCard = userCardObj => {
     const li = document.createElement('li')
     li.setAttribute("class", "card")
     li.innerHTML = `
-                    <div class="content">
-                        <h4>${userCardObj.character}</h4>
-                    </div>
-                    <div>
-                        <img src=${userCardObj.image}>
-                    </div>
+        <div class="content">
+            <h4>${userCardObj.character}</h4>
+        </div>
+        <div>
+            <img src=${userCardObj.image}>
+        </div>
 
-                    <div>
-                        <p class="quote">${userCardObj.quote}</p>
-                    </div>
+        <div>
+            <p class="quote">${userCardObj.quote}</p>
+        </div>
     `
-
-
-    indexCardUl.append(li)
+    allCardsUl.append(li)
 }
 
 const renderAllUsers = userArray => {
-    userArray.forEach(renderUser(userObj))
+    console.log(userArray)
 }
 
 const renderUser = userObj => {
-    // console.log(userObj)
-    profileDiv.innerHTML = `
+    console.log(userObj)
+    const userDiv = document.createElement("div")
+    userDiv.className = "user"
+    userDiv.dataset.id = userObj.id
+
+    userDiv.innerHTML = `
         <p>${userObj.name} </p>
         <p>${userObj.age} </p>
     `
-    indexCardUl.innerHTML = ``
+    allCardsUl.innerHTML = ``
     userObj.cards.forEach(renderUserCard)
+    userContainer.append(userDiv)
+}
+
+const renderAllCards = cardArray => {
+    cardArray.forEach(cardObj => renderOneCard(cardObj))
 }
 
 const renderOneCard = cardObj => {
     const li = document.createElement('li')
     li.setAttribute("class", "card")
     li.innerHTML = `
-                    <div class="content">
-                        <h4>${cardObj.character}</h4>
-                    </div>
-                    <div>
-                        <img src=${cardObj.image}>
-                    </div>
+        <div class="content">
+            <h4>${cardObj.character}</h4>
+        </div>
+        <div>
+            <img src=${cardObj.image}>
+        </div>
 
-                    <div>
-                        <p class="quote">${cardObj.quote}</p>
-                    </div>
+        <div>
+            <p class="quote">${cardObj.quote}</p>
+        </div>
     `
-
-
-    indexCardUl.append(li)
-
-
+    allCardsUl.append(li)
 }
 
-//change login to have a form textarea under it
+// ********** EVENT LISTENERS **********
+
 //user types in email and submits
 //if email matches any users emails, currentUserId = that user id
 // then in our event listener, we can call getUser(currentUserId)
-
 loginForm.addEventListener("submit", event => {
     event.preventDefault()
-    // debugger
-    console.log("login")
+    pageTitle.innerText = ""
+    pageTitle.innerText = "My Profile Page"
+
     //const email = event.target.email.value 
-    //if email.matches userObj.email
+    // if (event.target.matches(userObj.email)) {
+    //     console.log("success!")
+    // }
     //render that user's profile
-    // const userObj = 
-    getAllUsers()
+
+    // let clicked = false;
+    //   // Will only start if clicked is false
+    // if (!clicked) {
+    // // Set clicked to true
+    // clicked = true;
+
+    // // Do your processing here
+    // alert("You're already logged in!");
+
+    // // Re-enable after processing if you want
+    // // clicked = false;
+    // }
+
+    renderUser
+    getUser(3)
 })
+
+
 profileBtn.addEventListener("click", event => {
     event.preventDefault()
     console.log("My Profile")
@@ -118,12 +139,11 @@ profileBtn.addEventListener("click", event => {
     // write a function to render the user's cards/show page inside the div#card-container
         // if card.user_id === user.id
         // render card(s) to page
-    indexCardUl.innerHTML = ``
+    allCardsUl.innerHTML = ``
 
 //    getUser(2)
     //write function outside to render user cards
 })
-
 
 signupBtn.addEventListener("click", event => {
     event.preventDefault()
@@ -143,10 +163,18 @@ logoutBtn.addEventListener("click", event => {
     
 })
 
-//fetch all users
-//
+homeBtn.addEventListener("click", event => {
+    event.preventDefault()
+    console.log("Home!")
+    pageTitle.innerText = ""
+    pageTitle.innerText = "Simpsons Card Collection"
+    
+    getAllCards()
+})
 
-// fetches
+
+// ********** FETCH REQUESTS **********
+
 const getAllUsers = () => {
     fetch('http://localhost:3000/api/v1/users')
     .then(r => r.json())
@@ -172,7 +200,7 @@ const getUser = id => {
     .then(userObj => renderUser(userObj))
 }
 
-// initialize
+// ********** INITIALIZE **********
 
 // getUser(2)
 getCard(2)
